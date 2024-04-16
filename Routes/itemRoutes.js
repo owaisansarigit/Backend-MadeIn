b20c9bad7b32a78c07e56b8b2994bde9ad8b4281
owaisansarigit <filledstackdeveloper@gmail.com>
2024-04-16 13:10:36 +0530

    changes

--- Routes/itemRoutes.js
+++ Routes/itemRoutes.js
@@ -14,14 +14,15 @@ const {
 const router = express.Router();
 const multer = require("multer");
 const { verifyToken } = require("../Utils/jwt");
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "./uploads/");
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.originalname);
-  },
-});
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, "./uploads/");
+//   },
+//   filename: function (req, file, cb) {
+//     cb(null, file.originalname);
+//   },
+// });
+const storage = multer.diskStorage({});
 const upload = multer({ storage: storage });
 router.get("/", verifyToken, getItems);
 router.get("/:id", verifyToken, getItem);
